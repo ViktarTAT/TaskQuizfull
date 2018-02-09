9f66a0f791ffed32bf5906fce7043ba1f9095d4c
Balabusko Viktor <viktartat@gmail.com>
2018-02-09 13:08:08 +0200

    finished Account

--- src/test/java/by/htp/quizfullTask/domen/Account.java
+++ src/test/java/by/htp/quizfullTask/domen/Account.java
@@ -5,5 +5,110 @@ public class Account {
     private String password;
     private String repassword;
     private String email;
-    
+    private boolean corporate;
+
+    public Account() {
+
+    }
+
+    public Account(String login, String password, String repassword, String email, boolean corporate) {
+	super();
+	this.login = login;
+	this.password = password;
+	this.repassword = repassword;
+	this.email = email;
+	this.corporate = corporate;
+    }
+
+    public String getLogin() {
+	return login;
+    }
+
+    public void setLogin(String login) {
+	this.login = login;
+    }
+
+    public String getPassword() {
+	return password;
+    }
+
+    public void setPassword(String password) {
+	this.password = password;
+    }
+
+    public String getRepassword() {
+	return repassword;
+    }
+
+    public void setRepassword(String repassword) {
+	this.repassword = repassword;
+    }
+
+    public String getEmail() {
+	return email;
+    }
+
+    public void setEmail(String email) {
+	this.email = email;
+    }
+
+    public boolean isCorporate() {
+	return corporate;
+    }
+
+    public void setCorporate(boolean corporate) {
+	this.corporate = corporate;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + (corporate ? 1231 : 1237);
+	result = prime * result + ((email == null) ? 0 : email.hashCode());
+	result = prime * result + ((login == null) ? 0 : login.hashCode());
+	result = prime * result + ((password == null) ? 0 : password.hashCode());
+	result = prime * result + ((repassword == null) ? 0 : repassword.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Account other = (Account) obj;
+	if (corporate != other.corporate)
+	    return false;
+	if (email == null) {
+	    if (other.email != null)
+		return false;
+	} else if (!email.equals(other.email))
+	    return false;
+	if (login == null) {
+	    if (other.login != null)
+		return false;
+	} else if (!login.equals(other.login))
+	    return false;
+	if (password == null) {
+	    if (other.password != null)
+		return false;
+	} else if (!password.equals(other.password))
+	    return false;
+	if (repassword == null) {
+	    if (other.repassword != null)
+		return false;
+	} else if (!repassword.equals(other.repassword))
+	    return false;
+	return true;
+    }
+
+    @Override
+    public String toString() {
+	return "Account [login=" + login + ", password=" + password + ", repassword=" + repassword + ", email=" + email
+		+ ", corporate=" + corporate + "]";
+    }
 }
--- src/test/java/by/htp/quizfullTask/pages/RegistrationPage.java
+++ src/test/java/by/htp/quizfullTask/pages/RegistrationPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import by.htp.quizfullTask.domen.Account;
+
 public class RegistrationPage extends BasePage {
 
     @FindBy(id = "login")
@@ -14,29 +16,29 @@ public class RegistrationPage extends BasePage {
 
     @FindBy(name = "registrationForm.repassword")
     private WebElement inputRepassword;
-    
+
     @FindBy(name = "registrationForm.email")
     private WebElement inputEmail;
-    
+
     @FindBy(id = "corporate")
     private WebElement checkboxCorporate;
-    
+
     @FindBy(name = "registrationForm.captcha")
     private WebElement inputCaptcha;
 
     @FindBy(name = "ok")
     private WebElement buttonOk;
-    
+
     public RegistrationPage(WebDriver driver) {
 	super(driver);
     }
-    
-    public void createNewAcount(Account account){
-	inputLogin.sendKeys(login);
-	inputPassword.sendKeys(pass);
-	inputRepassword.sendKeys(repass);
-	inputEmail.sendKeys(email);
-	if(corporate)
+
+    public void createNewAcount(Account account) {
+	inputLogin.sendKeys(account.getLogin());
+	inputPassword.sendKeys(account.getPassword());
+	inputRepassword.sendKeys(account.getRepassword());
+	inputEmail.sendKeys(account.getEmail());
+	if (account.isCorporate())
 	    checkboxCorporate.click();
 	inputCaptcha.click();
 	buttonOk.click();
